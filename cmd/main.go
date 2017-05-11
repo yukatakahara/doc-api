51be6d586b69d27f724de82f53e542c0ab2e5fcb
Oren <orengolan@gmail.com>
2017-05-11 00:01:27 -0700

    pass email and password

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,53 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/cayleygraph/cayley"
 	"github.com/cayleygraph/cayley/graph"
 	_ "github.com/cayleygraph/cayley/graph/bolt"
 	"github.com/cayleygraph/cayley/quad"
+	uuid "github.com/satori/go.uuid"
 )
+
+var ErrBadFormat = errors.New("invalid email format")
+var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func main() {
 	// Some globally applicable things
 	graph.IgnoreMissing = true
 	graph.IgnoreDuplicates = true
 
-	// File for your new BoltDB. Use path to regular file and not temporary in the real world
-	// t := getTempfileName()
-	// fmt.Printf("%v\n", t)
-
-	// defer os.Remove(t)                 // clean up
-	// store := initializeAndOpenGraph(t) // initialize the graph
-
 	dbFile := flag.String("db", "db.boltdb", "BoltDB file")
+	email := flag.String("email", "", "email")
+	password := flag.String("password", "", "password")
 	flag.Parse()
 
 	store := initializeAndOpenGraph(dbFile)
 
-	createAdmin(store) // add quads to the graph
+	createAdmin(store, *email, *password) // add quads to the graph
+	fmt.Println(*email)
 
-	countOuts(store, "41234214")
-	lookAtOuts(store, "41234214")
-	lookAtIns(store, "41234214")
+	// countOuts(store, "41234214")
+	// lookAtOuts(store, "41234214")
+	// lookAtIns(store, "41234214")
+}
 
-	// lookAtOuts(store, "jorgent")
-	// lookAtIns(store, "jorgent")
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
 
-	// lookAtFriendsOfFriends(store, "barakmich")
-
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func initializeAndOpenGraph(dbFile *string) *cayley.Handle {
@@ -54,24 +62,35 @@ func initializeAndOpenGraph(dbFile *string) *cayley.Handle {
 	return store
 }
 
-func getTempfileName() string {
-	tmpfile, err := ioutil.TempFile("", "example")
+func createAdmin(store *cayley.Handle, email string, password string) {
+	err := ValidateFormat(email)
+
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	return tmpfile.Name()
+	uuid := uuid.NewV1().String()
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	store.AddQuad(quad.Make(uuid, "is_a", "admin", nil))
+	store.AddQuad(quad.Make(uuid, "email", email, nil))
+	store.AddQuad(quad.Make(uuid, "hashed_password", hash, nil))
+
+	countOuts(store, uuid)
+	lookAtOuts(store, uuid)
 }
 
-func createAdmin(store *cayley.Handle) {
-	store.AddQuad(quad.MakeRaw("41234214", "is_a", "admin", ""))
-	store.AddQuad(quad.MakeRaw("41234214", "email", "foo@gmail.com", ""))
-	store.AddQuad(quad.MakeRaw("41234214", "hashed_password", "aqjxkjx", ""))
+func ValidateFormat(email string) error {
+	if !emailRegexp.MatchString(email) {
+		return ErrBadFormat
+	}
+	return nil
 }
 
 // countOuts ... well, counts Outs
 func countOuts(store *cayley.Handle, to string) {
-	p := cayley.StartPath(store, quad.Raw(to)).Out().Count()
+	p := cayley.StartPath(store, quad.String(to)).Out().Count()
 	fmt.Printf("\n\ncountOuts for %s: ", to)
 	p.Iterate(nil).EachValue(store, func(v quad.Value) {
 		fmt.Printf("%d\n", quad.NativeOf(v))
@@ -81,7 +100,7 @@ func countOuts(store *cayley.Handle, to string) {
 
 // lookAtOuts looks at the outbound links from the "to" node
 func lookAtOuts(store *cayley.Handle, to string) {
-	p := cayley.StartPath(store, quad.Raw(to)) // start from a single node, but we could start from multiple
+	p := cayley.StartPath(store, quad.String(to)) // start from a single node, but we could start from multiple
 
 	// this gives us a path with all the output predicates from our starting point
 	p = p.Tag("subject").OutWithTags([]string{"predicate"}).Tag("object")
