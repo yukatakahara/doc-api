3ca98df99b6712c1837a113baff4c569b8109708
Oren <orengolan@gmail.com>
2017-05-12 21:02:10 -0700

    misc

---
+++ admin.go
@@ -0,0 +1,105 @@
+package admin
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/cayleygraph/cayley"
+	"github.com/cayleygraph/cayley/graph"
+	_ "github.com/cayleygraph/cayley/graph/bolt"
+	"github.com/cayleygraph/cayley/quad"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrBadFormat = errors.New("invalid email format")
+var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+type Admin struct {
+	Email          string `json:"email"`
+	HashedPassword string `json:"hashedPassword"`
+}
+
+func New() (*Admin, error) {
+	a := &Admin{}
+
+	return a, nil
+}
+
+func initializeAndOpenGraph(dbFile *string) *cayley.Handle {
+	graph.InitQuadStore("bolt", *dbFile, nil)
+
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", *dbFile, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return store
+}
+
+func (p *Admin) Create(email string, password string) Admin {
+	dbFile := flag.String("db", "db.boltdb", "BoltDB file")
+	store := initializeAndOpenGraph(dbFile)
+
+	fmt.Println("email", email)
+	// create new admin in the db
+	err := ValidateFormat(email)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	uuid := uuid.NewV1().String()
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	store.AddQuad(quad.Make(uuid, "is_a", "admin", nil))
+	store.AddQuad(quad.Make(uuid, "email", email, nil))
+	store.AddQuad(quad.Make(uuid, "hashed_password", hash, nil))
+
+	a := Admin{email, hash}
+
+	return a
+}
+
+func ValidateFormat(email string) error {
+	if !emailRegexp.MatchString(email) {
+		return ErrBadFormat
+	}
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func (a *Admin) All() []Admin {
+	// get admins from the db
+	dbFile := flag.String("db", "db.boltdb", "BoltDB file")
+	store := initializeAndOpenGraph(dbFile)
+	var err error
+
+	p := cayley.StartPath(store).Has(quad.String("is_a"), quad.String("admin")).Out()
+
+	results := []Admin{}
+	err = p.Iterate(nil).EachValue(nil, func(value quad.Value) {
+		nativeValue := quad.NativeOf(value) // this converts RDF values to normal Go types
+		fmt.Println(nativeValue)
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	results = append(results, Admin{"foo@gmail.com", "mbmbjkjk"})
+	results = append(results, Admin{"bar@gmail.com", "54354353"})
+
+	return results
+}
--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,136 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"regexp"
 
-	"golang.org/x/crypto/bcrypt"
-
-	"github.com/cayleygraph/cayley"
-	"github.com/cayleygraph/cayley/graph"
 	_ "github.com/cayleygraph/cayley/graph/bolt"
-	"github.com/cayleygraph/cayley/quad"
-	uuid "github.com/satori/go.uuid"
+	"github.com/oren/doc-api"
 )
 
 var ErrBadFormat = errors.New("invalid email format")
 var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func main() {
-	// Some globally applicable things
-	graph.IgnoreMissing = true
-	graph.IgnoreDuplicates = true
-
-	dbFile := flag.String("db", "db.boltdb", "BoltDB file")
 	email := flag.String("email", "", "email")
-	password := flag.String("password", "", "password")
+	// password := flag.String("password", "", "password")
 	flag.Parse()
+	fmt.Println("email", *email)
+	os.Exit(0)
 
-	store := initializeAndOpenGraph(dbFile)
+	arg := os.Args[1]
 
-	createAdmin(store, *email, *password) // add quads to the graph
-	listAdmins(store)
-}
-
-func listAdmins(store *cayley.Handle) {
-	var err error
-
-	p := cayley.StartPath(store).Has(quad.String("is_a"), quad.String("admin")).Out()
-
-	err = p.Iterate(nil).EachValue(nil, func(value quad.Value) {
-		nativeValue := quad.NativeOf(value) // this converts RDF values to normal Go types
-		fmt.Println(nativeValue)
-	})
-
+	Admin, err := admin.New()
 	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
-
-func initializeAndOpenGraph(dbFile *string) *cayley.Handle {
-	graph.InitQuadStore("bolt", *dbFile, nil)
-
-	// Open and use the database
-	store, err := cayley.NewGraph("bolt", *dbFile, nil)
-	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
-	return store
-}
+	switch arg {
+	case "create-admin":
+		email := flag.String("email", "", "email")
+		password := flag.String("password", "", "password")
+		flag.Parse()
 
-func createAdmin(store *cayley.Handle, email string, password string) {
-	err := ValidateFormat(email)
+		fmt.Println("email", *email)
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		results := Admin.Create(*email, *password)
+		data, err := json.Marshal(results)
+
+		if err != nil {
+			fmt.Errorf("encode response: %v", err)
+		}
+
+		os.Stdout.Write(data)
+		os.Exit(0)
+	case "list-admins":
+		results := Admin.All()
+		data, err := json.Marshal(results)
+
+		if err != nil {
+			fmt.Errorf("encode response: %v", err)
+		}
+
+		os.Stdout.Write(data)
+		os.Exit(0)
+	default:
+		fmt.Println("Wrong argument")
+		os.Exit(0)
 	}
 
-	uuid := uuid.NewV1().String()
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+	// results := Admin.Create("foobar@gmail.com", "password11")
+	// // results := Admin.All()
+	// data, err := json.Marshal(results)
+	// if err != nil {
+	// 	fmt.Errorf("encode response: %v", err)
+	// }
+	// os.Stdout.Write(data)
 
-	store.AddQuad(quad.Make(uuid, "is_a", "admin", nil))
-	store.AddQuad(quad.Make(uuid, "email", email, nil))
-	store.AddQuad(quad.Make(uuid, "hashed_password", hash, nil))
+	// Some globally applicable things
+	// graph.IgnoreMissing = true
+	// graph.IgnoreDuplicates = true
 
-	// countOuts(store, uuid)
-	// lookAtOuts(store, uuid)
-}
+	// dbFile := flag.String("db", "db.boltdb", "BoltDB file")
+	// email := flag.String("email", "", "email")
+	// password := flag.String("password", "", "password")
+	// flag.Parse()
 
-func ValidateFormat(email string) error {
-	if !emailRegexp.MatchString(email) {
-		return ErrBadFormat
-	}
-	return nil
-}
+	// store := initializeAndOpenGraph(dbFile)
 
-// countOuts ... well, counts Outs
-func countOuts(store *cayley.Handle, to string) {
-	p := cayley.StartPath(store, quad.String(to)).Out().Count()
-	fmt.Printf("\n\ncountOuts for %s: ", to)
-	p.Iterate(nil).EachValue(store, func(v quad.Value) {
-		fmt.Printf("%d\n", quad.NativeOf(v))
-	})
-	fmt.Printf("============================================\n")
-}
-
-// lookAtOuts looks at the outbound links from the "to" node
-func lookAtOuts(store *cayley.Handle, to string) {
-	p := cayley.StartPath(store, quad.String(to)) // start from a single node, but we could start from multiple
-
-	// this gives us a path with all the output predicates from our starting point
-	p = p.Tag("subject").OutWithTags([]string{"predicate"}).Tag("object")
-
-	fmt.Printf("\nlookAtOuts: subject (%s) -predicate-> object\n", to)
-	fmt.Printf("============================================\n")
-
-	p.Iterate(nil).TagValues(nil, func(m map[string]quad.Value) {
-		fmt.Printf("%s `%s`-> %s\n", m["subject"], m["predicate"], m["object"])
-	})
-}
-
-// lookAtIns looks at the inbound links to the "to" node
-func lookAtIns(store *cayley.Handle, to string) {
-	fmt.Printf("\nlookAtIns: object <-predicate- subject (%s)\n", to)
-	fmt.Printf("=============================================\n")
-
-	cayley.StartPath(store, quad.Raw(to)).Tag("object").InWithTags([]string{"predicate"}).Tag("subject").Iterate(nil).TagValues(nil, func(m map[string]quad.Value) {
-		fmt.Printf("%s <-`%s` %s\n", m["object"], m["predicate"], m["subject"])
-	})
-
+	// createAdmin(store, *email, *password) // add quads to the graph
+	// listAdmins(store)
 }
