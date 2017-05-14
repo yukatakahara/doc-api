4a8d68d9b7c1c41c511d3134a661f5cce0c475a8
Oren <orengolan@gmail.com>
2017-05-13 23:01:42 -0700

    split to multiple files

--- admin.go
+++ admin.go
@@ -8,10 +8,6 @@ import (
 	"github.com/cayleygraph/cayley"
 	"github.com/cayleygraph/cayley/graph"
 	_ "github.com/cayleygraph/cayley/graph/bolt"
-	"github.com/cayleygraph/cayley/quad"
-	uuid "github.com/satori/go.uuid"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 var dbPath = "/tmp/db.boltdb"
@@ -47,80 +43,4 @@ func initializeAndOpenGraph(dbFile string) *cayley.Handle {
 	}
 
 	return store
-}
-
-// TODO: Check for duplicate email
-// TODO: Use lock to make sure between check and write we don't have one slip in
-// func CreateAdmin(h *cayley.Handle, a Admin) error {
-func (a *Admin) Create() error {
-	h := initializeAndOpenGraph(dbPath)
-	err := validateEmail(a.Email)
-	if err != nil {
-		return err
-	}
-
-	a.HashedPassword, err = hashPassword(a.Password)
-	if err != nil {
-		return err
-	}
-
-	uuid := uuid.NewV1().String()
-
-	t := cayley.NewTransaction()
-	t.AddQuad(quad.Make(quad.IRI(uuid), quad.IRI("is_a"), quad.String("admin"), nil))
-	t.AddQuad(quad.Make(quad.IRI(uuid), quad.IRI("email"), quad.String(a.Email), nil))
-	t.AddQuad(quad.Make(quad.IRI(uuid), quad.IRI("hashed_password"), quad.String(a.HashedPassword), nil))
-	err = h.ApplyTransaction(t)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func validateEmail(email string) error {
-	if !emailRegexp.MatchString(email) {
-		return ErrBadFormat
-	}
-	return nil
-}
-
-func hashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-// get admins from the db
-func (a *Admin) All() []Admin {
-	h := initializeAndOpenGraph(dbPath)
-
-	As, err := ReadAdmins(h, regexp.MustCompile(".*"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return As
-
-}
-
-func ReadAdmins(h *cayley.Handle, email *regexp.Regexp) ([]Admin, error) {
-	p := cayley.StartPath(h).Tag("id").
-		Out(quad.IRI("email")).Regex(email).In(quad.IRI("email")).Has(quad.IRI("is_a"), quad.String("admin")).
-		Save(quad.IRI("email"), "email").
-		Save(quad.IRI("hashed_password"), "hashed_password")
-
-	results := []Admin{}
-	err := p.Iterate(nil).TagValues(nil, func(tags map[string]quad.Value) {
-		results = append(results, Admin{
-			ID:             quad.NativeOf(tags["id"]).(quad.IRI).String(),
-			Email:          quad.NativeOf(tags["email"]).(string),
-			HashedPassword: quad.NativeOf(tags["hashed_password"]).(string),
-		})
-	})
-	if err != nil {
-		return []Admin{}, err
-	}
-	return results, nil
 }
