6069ba3c5a088535b322194f7920f5588a3a05b8
Oren <orengolan@gmail.com>
2017-05-28 18:34:21 -0700

    add Quad to AdminService

---
+++ bolt/quads.go
@@ -0,0 +1,30 @@
+package bolt
+
+import (
+	"github.com/cayleygraph/cayley"
+	"github.com/cayleygraph/cayley/quad"
+)
+
+// get all quads from the db
+// func (a *AdminService) Quads() ([]interface{}, error) {
+func (a *AdminService) Quads() ([]quad.Quad, error) {
+	results, err := readAllQuads(a.Store)
+
+	if err != nil {
+		return results, err
+	}
+
+	return results, nil
+}
+
+func readAllQuads(store *cayley.Handle) ([]quad.Quad, error) {
+	var results []quad.Quad
+	it := store.QuadsAllIterator()
+	defer it.Close()
+
+	for it.Next() {
+		results = append(results, store.Quad(it.Result()))
+	}
+
+	return results, nil
+}
